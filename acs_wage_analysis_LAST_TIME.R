41b1015fd647334ba32668a6ca3382a92364c7fa
Ben Zipperer <benzipperer@gmail.com>
2020-10-13 08:51:26 -0400

    Add files via upload

---
+++ acs_wage_analysis_LAST_TIME.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(haven)
+
+# Load the ACS data from IPUMS
+acs <- read_dta("/home/benzipperer/Downloads/acs_2018.dta")
+
+# Clean up the data
+acs_clean <- acs %>% 
+  # keep only workers
+  filter(incwage > 0 & incwage < 999998) %>% 
+  filter(uhrswork > 0) %>% 
+  # keep only full-time workers
+  filter(wkswork2 == 6) %>% 
+  # restrict analysis to VA
+  filter(statefip == 51) %>% 
+  # define wages and low-wage workers
+  mutate(wage = incwage / (uhrswork * 51)) %>% 
+  mutate(low_wage = wage <= 15)
+
+# Do the analysis
+acs_clean %>% 
+  summarize(weighted.mean(low_wage, perwt))
+
+# by gender
+acs_clean %>%
+  group_by(sex) %>% 
+  summarize(weighted.mean(low_wage, perwt))
+
+acs_clean %>%
+  group_by(race) %>% 
+  summarize(weighted.mean(low_wage, perwt))
